a790be581f0b3440bec5122dd0bff8e41e1cd8d6
darkgsl <54315640+darkgsl@users.noreply.github.com>
2021-05-15 19:44:41 +0300

    feat: finish  exercise 4

--- Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
@@ -3,7 +3,34 @@
 @implementation TwoDimensionalArraySorter
 
 - (NSArray *)twoDimensionalSort:(NSArray<NSArray *> *)array {
-    return @[];
+    
+//limit: subarray contains  values with same type
+//check nil and empty
+    //проверяем 2-х мерность массива
+    if ((array.count == 0) || ([array[0] isKindOfClass:NSArray.class] == NO)){
+        return @[];
+    }
+    
+    NSMutableArray *numbers = [NSMutableArray new];
+    NSMutableArray *words = [NSMutableArray new];
+    for (NSArray *subArray in array) {
+        if ([subArray[0] isKindOfClass:NSNumber.class] ) {
+            [numbers addObjectsFromArray:subArray];
+        } else if ( [subArray[0] isKindOfClass:NSString.class] ) {
+            [words addObjectsFromArray:subArray];
+        } else {
+            return @[];
+        }
+    }
+
+    if (numbers.count == 0 ) {
+        return [words sortedArrayUsingSelector:@selector(compare:)];
+    } else if (words.count == 0) {
+        return [numbers sortedArrayUsingSelector:@selector(compare:)];
+    }
+    
+   NSArray *result = [NSArray arrayWithObjects:[numbers sortedArrayUsingSelector:@selector(compare:)],[words sortedArrayUsingSelector:@selector(compare:)], nil];
+    return result;
 }
 
 @end
