e482c11c8d8226e5d491f577fdec628da9ec3d1d
darkgsl <54315640+darkgsl@users.noreply.github.com>
2021-05-15 18:18:16 +0300

    feat: finish  exercise 1

--- Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
@@ -1,5 +1,30 @@
 #import "ReverseBinary.h"
 
 UInt8 ReverseInteger(UInt8 n) {
-    return 0;
+    if (n <= 0) {
+        return  0;
+    }
+    UInt8 value = n;
+    NSMutableArray *array = [NSMutableArray new];
+    //make binary
+    while (value) {
+        [array addObject: [NSNumber numberWithUnsignedShort: value % 2]];
+        value /= 2;
+    }
+
+    //check length
+    if (array.count < 8) {
+        NSUInteger count = 8 - array.count;
+        for (NSUInteger i = 0; i < count; i += 1) {
+            [array addObject: [NSNumber numberWithUnsignedShort: 0]];
+        }
+        
+    }
+    
+    //make integer
+    for ( NSNumber *number in array) {
+        value = value * 2 + number.shortValue;
+    }
+
+    return value;
 }
