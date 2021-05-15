ade6dc501e46eb98b7dc9f07d9eb3bad236e438f
darkgsl <54315640+darkgsl@users.noreply.github.com>
2021-05-15 18:59:33 +0300

    feat: finish  exercise 2-3

--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -3,7 +3,15 @@
 @implementation Pairs
 
 - (NSInteger)countPairs:(NSArray <NSNumber *> *)array number:(NSNumber *)number {
-    return 0;
+    //limits: only unique value
+    NSUInteger result = 0;
+    NSSet *set = [[NSSet alloc] initWithArray:array];
+    for (NSNumber *value in array) {
+        if ([set containsObject:[NSNumber numberWithInteger: value.integerValue - number.integerValue]]) {
+            result += 1;
+        }
+    }
+    return result;
 }
 
 @end
--- Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
@@ -3,7 +3,32 @@
 @implementation IPFormatConverter
 
 - (NSString *)ipFormatConverter:(NSArray *)numbersArray {
-    return nil;
+    
+    if (numbersArray.count == 0){
+        return @"";
+    }
+    
+    for (NSNumber *value in numbersArray) {
+        NSInteger number = value.integerValue;
+        if (number > 255) {
+            return @"The numbers in the input array can be in the range from 0 to 255.";
+        } else if (number < 0) {
+            return @"Negative numbers are not valid for input.";
+        }
+    }
+    
+    NSMutableArray *array = [NSMutableArray arrayWithArray:numbersArray];
+    
+    //check length
+    if (numbersArray.count < 4) {
+        NSUInteger count = 4 - array.count;
+        for (NSUInteger i = 0; i < count; i += 1) {
+            [array addObject: [NSNumber numberWithUnsignedShort: 0]];
+        }
+    }
+    
+    return [NSString stringWithFormat:@"%d.%d.%d.%d", [array[0] integerValue], [array[1] integerValue], [ array[2] integerValue], [array[3] integerValue]];
+
 }
 
 @end
